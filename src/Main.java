db5c67cd2cdcfe2983723f782360624b2675656b
Ильмира <kushkarova.1998@mail.ru>
2023-06-18 21:13:59 +0300

    Homework 5.1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //task1
+        for (int i = 1; i < 11; i = i + 1) ;
+        {
+            System.out.println("Интерация цикла " + i);
+        }
+    }
+}
